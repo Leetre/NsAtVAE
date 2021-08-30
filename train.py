44e13da174c3c3f1bfbd8353c52a774e6d26eeb3
Lee <37473694+Leetre@users.noreply.github.com>
2021-08-30 17:20:01 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,11 +2,15 @@ import argparse, yaml
 import numpy as np
 from model import *
 import loss
+from torch.optim.lr_scheduler import LambdaLR
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, default='config.yml')
 parser.add_argument('--datapath', type=str, default='data/valve1')
-parser.add_argument('--epochs', type=int, default=20)
+parser.add_argument('--epochs', type=int, default=10)
+
+def decay(x):
+    return 0.01 + (0.99)*(0.9999)**x
 
 def load_data(path):
     data = np.load(path+'/'+'data.npy')
@@ -28,8 +32,9 @@ def train(args):
     model = load_model(config_f)
     print(model)
 
-    # optimizer
-    optimizer = torch.optim.Adam(model.parameters())
+    # optimizer and loss
+    optimizer = torch.optim.Adam(model.parameters(), 1e-3)
+    scheduler = LambdaLR(optimizer, decay)
 
     # training
     for epoch in range(epochs):
@@ -40,7 +45,7 @@ def train(args):
             current_data = torch.tensor(data[num], dtype=torch.float)
             # print(current_data)
             error_c, energy, error_r, _, mu, sigma = model(tuple((last_mu, last_sigma, current_data)))
-            # print(error_r)
+            # print(energy)
             mu.detach_()
             sigma.detach_()
             current_loss = loss.Loss(error_c, energy, error_r)
@@ -49,10 +54,11 @@ def train(args):
             optimizer.zero_grad()
             current_loss.backward()
             optimizer.step()
+            scheduler.step()
 
             print(("epoch %d: data %d: current loss is %.5f")%(epoch+1, num, current_loss.item()))
 
-        torch.save(model.state_dict(), 'model/epoch'+str(epoch+1)+'.pth')
+        torch.save(model.state_dict(), 'model/epoch'+str(epoch+1)+'SKAB1'+'.pth')
     # print(model)
     # print(data.shape[0])
 
